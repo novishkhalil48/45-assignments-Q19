4fa6f6cb30983e16f071712ee4f028f25bed71b2
Novish Khalil <novishkhalil2@hotmail.com>
2024-11-11 00:29:38 +0500

    Add files via upload

---
+++ Q19.js
@@ -0,0 +1,5 @@
+"use strict";
+// 19:Dinner Guest:Working with one of the programs from exercise You created dinner table guest list,print amessage indicating the number of people you're inviting to dinner.
+let guests = ["Novish", "Yusra", "Bushra", "Rabab"];
+let numOfGuests = guests.length;
+console.log("You are inviting" + " " + numOfGuests + " people to dinner");
---
+++ Q19.ts
@@ -0,0 +1,5 @@
+// 19:Dinner Guest:Working with one of the programs from exercise You created dinner table guest list,print amessage indicating the number of people you're inviting to dinner.
+
+let guests:string[] = ["Novish","Yusra", "Bushra", "Rabab"]
+let numOfGuests:number = guests.length
+console.log("You are inviting" + " " + numOfGuests + " people to dinner")
